1484553d80742b6ddefffc3a04197a22ce8602f3
Mesih Grmay <mesihgrmay@yahoo.com>
2022-07-14 14:43:43 -0700

    pint Task 1

---
+++ pint.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+/**
+  * pint - prints an element from the top of the stack
+  * @stack: pointer to the top of the stack list
+  * @line_number: line number
+  * Return: void
+  */
+
+void pint(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = *stack;
+	if (*stack == NULL || current == NULL)
+		show_pint_error_msg(line_number);
+	printf("%d\n", current->n);
+}
