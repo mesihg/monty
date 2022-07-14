ea38766e5594a951b4963b575d13c45fc6bdada4
Mesih Grmay <mesihgrmay@yahoo.com>
2022-07-14 16:27:06 -0700

    nop Task 5

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+  * nop - do nothing
+  * @stack: pointer to the top of the stack list
+  * @line_number: line number
+  * Return: void
+  */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void) *stack;
+	(void) line_number;
+}
