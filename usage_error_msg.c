6692d2d2200a0567572bc013a74a44ad72d5787d
Mesih Grmay <mesihgrmay@yahoo.com>
2022-07-14 15:12:27 -0700

    added new function

--- usage_error_msg.c
+++ usage_error_msg.c
@@ -35,3 +35,14 @@ void show_pint_error_msg(unsigned int line_number)
 	fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 	exit(EXIT_FAILURE);
 }
+
+/**
+  * show_pop_error_msg - prints error if given input is invalid
+  * @line_number: line number where the error occurred
+  * Return: void
+  */
+void show_pop_error_msg(unsigned int line_number)
+{
+	fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+	exit(EXIT_FAILURE);
+}
