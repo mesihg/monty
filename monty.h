6ccaf3ad9867d0803adbd2151f347a100f0ebebf
Mesih Grmay <mesihgrmay@yahoo.com>
2022-07-14 16:36:37 -0700

    Header files

--- monty.h
+++ monty.h
@@ -74,6 +74,7 @@ void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
 void free_stack(stack_t *h);
 void opt_runner(char *opt_content, stack_t **stack, unsigned int line_number);
 
