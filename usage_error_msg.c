8c7b72ad8d24fb58372c400c129427bb0eb4dcbd
Mesih Grmay <mesihgrmay@yahoo.com>
2022-07-14 15:56:17 -0700

    bug fix

--- usage_error_msg.c
+++ usage_error_msg.c
@@ -46,3 +46,14 @@ void show_pop_error_msg(unsigned int line_number)
 	fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 	exit(EXIT_FAILURE);
 }
+
+/**
+  * show_swap_error_msg - prints error if given input is invalid
+  * @line_number: line number where the error occurred
+  * Return: void
+  */
+void show_swap_error_msg(line_number)
+{
+	fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+	exit(EXIT_FAILURE);
+}
