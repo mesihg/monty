9a80cb90689e2b4c3e2f38b002cfd4010a9f073d
Mesih Grmay <mesihgrmay@yahoo.com>
2022-07-14 16:02:15 -0700

    bug fix

--- usage_error_msg.c
+++ usage_error_msg.c
@@ -52,7 +52,7 @@ void show_pop_error_msg(unsigned int line_number)
   * @line_number: line number where the error occurred
   * Return: void
   */
-void show_swap_error_msg(line_number)
+void show_swap_error_msg(unsigned int line_number)
 {
 	fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 	exit(EXIT_FAILURE);
