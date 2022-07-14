c79cf2974d7137f977e16f5ff9527746d201c802
Mesih Grmay <mesihgrmay@yahoo.com>
2022-07-14 15:12:52 -0700

    pop Task 2

---
+++ pop.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+
+/**
+  * pop - remove an element from the top of the stack
+  * @stack: pointer to the top of the stack list
+  * @line_number: line number
+  * Return: void
+  */
+
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current;
+
+	if (*stack == NULL)
+		show_pop_error_msg(line_number);
+	current = (*stack)->next;
+	if ((*stack)->next != NULL)
+		(*stack)->next->prev = NULL;
+	free(*stack);
+	*stack = current;
+}
