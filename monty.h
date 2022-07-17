bd16047a5ceb1a68cdc00a4173909e2598d07b36
Mesih Grmay <mesihgrmay@yahoo.com>
2022-07-17 14:42:22 -0700

    Header files

--- monty.h
+++ monty.h
@@ -81,6 +81,7 @@ void m_div(stack_t **stack, unsigned int line_number);
 void mul(stack_t **stack, unsigned int line_number);
 void mod(stack_t **stack, unsigned int line_number);
 void pchar(stack_t **stack, unsigned int line_number);
+void pstr(stack_t **stack, unsigned int line_number);
 void free_stack(stack_t *h);
 void opt_runner(char *opt_content, stack_t **stack, unsigned int line_number);
 
