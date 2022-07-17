e7ca066fb814a922c995497c6f75df222a545757
Mesih Grmay <mesihgrmay@yahoo.com>
2022-07-17 12:36:32 -0700

    push, pall Task 0

--- push.c
+++ push.c
@@ -11,21 +11,27 @@ void push(stack_t **stack, unsigned int line_number)
 {
 	stack_t *new_node;
 	char *val = arg.data;
-	int i, data;
+	int i = 0, f = 0, n;
 
-	if (val == NULL)
+	if (arg.data)
+	{
+		if (arg.data[0] == '-')
+			i++;
+		for (; arg.data[i] != '\0'; i++)
+		{
+			if (arg.data[i] > 57 || arg.data[i] < 48)
+				f = 1;
+		}
+		if (f == 1)
+			show_invalid_input_error_msg(line_number);
+	}
+
+
+	else
 	{
 		show_invalid_input_error_msg(line_number);
 	}
-
-	for (i = 0; val[i] != '\0'; i++)
-	{
-		if (isdigit(val[i]) == 0 && val[i] != '-')
-		{
-			show_invalid_input_error_msg(line_number);
-		}
-	}
-	data = atoi(val);
+	n = atoi(arg.data);
 	new_node = malloc(sizeof(stack_t));
 	if (new_node == NULL)
 	{
@@ -33,7 +39,7 @@ void push(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 
-	new_node->n = data;
+	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = *stack;
 
